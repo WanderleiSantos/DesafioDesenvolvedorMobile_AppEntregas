a3c0a0acf8a69609eb69eab16a8eba5151a3528f
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2025-01-06 18:37:52 -0300

    feat: Cria componente DeliveryTextField personalizado

---
+++ app/src/main/java/com/luizalabs/labsentregas/ui/components/DeliveryTextField.kt
@@ -0,0 +1,118 @@
+package com.luizalabs.labsentregas.ui.components
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.luizalabs.labsentregas.ui.theme.CornflowerBlue
+
+@Composable
+fun DeliveryTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    prefix: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = RoundedCornerShape(10.dp),
+    colors: TextFieldColors = OutlinedTextFieldDefaults.colors().copy(
+        focusedIndicatorColor = CornflowerBlue,
+        unfocusedIndicatorColor = Color.LightGray.copy(alpha = 0.4f),
+        unfocusedTextColor = Color.DarkGray,
+        focusedTextColor = Color.DarkGray,
+    ),
+    fontSize: TextUnit = 16.sp,
+) {
+    Column(Modifier.padding(vertical = 8.dp)) {
+        label?.let {
+            Row {
+                Spacer(modifier = Modifier.size(4.dp))
+                it()
+            }
+        }
+        Spacer(modifier = Modifier.size(8.dp))
+        OutlinedTextField(
+            value = value,
+            onValueChange,
+            modifier,
+            enabled,
+            readOnly,
+            textStyle.copy( fontSize = fontSize),
+            null,
+            placeholder,
+            leadingIcon,
+            trailingIcon,
+            prefix,
+            suffix,
+            supportingText,
+            isError,
+            visualTransformation,
+            keyboardOptions,
+            keyboardActions,
+            singleLine,
+            maxLines,
+            minLines,
+            interactionSource,
+            shape,
+            colors
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewDeliveryTextField() {
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        DeliveryTextField(
+            value = "Wanderlei Santos",
+            onValueChange = {},
+            label = { Text("Cliente") },
+            isError = false,
+            fontSize = 16.sp
+        )
+        DeliveryTextField(
+            value = "Com Erro",
+            onValueChange = {},
+            label = { Text("Cliente") },
+            isError = true,
+            fontSize = 16.sp
+        )
+    }
+}
